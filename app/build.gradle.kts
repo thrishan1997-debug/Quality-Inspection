344505d25de876d26bfce0089353f59c68f81752
thrishan1997-debug <thrishan1997@gmail.com>
2026-09-20 19:19:32 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.lifelong.supplierquality"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.lifelong.supplierquality"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+}
