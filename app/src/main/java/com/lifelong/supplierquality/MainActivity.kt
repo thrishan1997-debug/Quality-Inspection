2bca78c70e7e30c67781464a3fea0f71f51f1848
thrishan1997-debug <thrishan1997@gmail.com>
2026-09-20 19:23:04 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/lifelong/supplierquality/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.lifelong.supplierquality
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.webViewClient = WebViewClient()
+
+        setContentView(webView)
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
